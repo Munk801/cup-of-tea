140006c831c406daa24e9c4527c74ff61acda767
stephenl <stephenl@blur.com>
2015-03-31 19:24:52 -0700

    Added support for old xml style prefs.  Correctly converts this to json format while maintaining data integrity

--- __init__.py
+++ __init__.py
@@ -4,16 +4,52 @@ import json
 import os
 import sys
 
+from blurdev import prefs as bp
+
 class Prefs(object):
 
 	def __init__(self, path="", data={}):
 		self.path = path
 		self._data = data
 		if self.path:
-			with open(self.path, 'r') as fp:
-				self._data = json.load(fp)
+			try:
+				with open(self.path, 'r') as fp:
+					self._data = json.load(fp)
+			except ValueError as e:
+				# This may be an xml
+				self._data = self.initializeXML()
 		self.ignore = self.__dict__.keys()
 		self.ignore.append('ignore')
+
+	def initializeXML(self):
+		# Use the old preference object ot pull the xml data
+		oldprefs = bp.Preference()
+		oldprefs.load(self.path)
+		data = self.extractXMLData(oldprefs)
+		return data
+
+	def extractXMLData(self, prefs):
+		# Retrieve the root and store everything under the root
+		rootkey =  prefs.root().name()
+		rootvalue = self.traverse(prefs.root())
+		return {rootkey : rootvalue}
+
+	def traverse(self, root):
+		data = {}
+		element = root
+		# Add all the top level items for that element
+		for key, value in root.attributeDict().iteritems():
+			# Attempt to store the evaluation of the xml string
+			try:
+				data[key] = eval(value)
+			except NameError as e:
+				data[key] = value
+		# Recursively traverse through children
+		if element.children():
+			for child in element.children():
+				childkey = child.name()
+				data[childkey] = self.traverse(child)
+		return data
 
 	def __str__(self):
 		return json.dumps(
