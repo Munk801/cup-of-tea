bc12c48187dca6676014109f46d37a41db3280c0
stephenl <stephenl@blur.com>
2015-04-01 18:54:43 -0700

    Added find function to search for prefs. Also conformed to better ingest old prefs. Fixed small bug with evaling types

--- __init__.py
+++ __init__.py
@@ -4,20 +4,62 @@ import json
 import os
 import sys
 
+import blurdev
 from blurdev import prefs as bp
+from blurdev import osystem
+
+def find(name, reload=False, coreName='', shared=False, index=0):
+	"""
+	Finds a preference for the with the inputed name.  If a pref already
+	exists within the cache, then the cached pref is returned; otherwise,
+	it is loaded from the blurdev preference location.
+
+	:param name: the name of the preference to retrieve
+	:type name: str
+	:param reload: reloads the cached item
+	:type reload: bool
+	:param coreName: specify a specific core name to save with.
+	:type coreName: str
+	:param shared: save to the network path not localy. Defaults to False
+	:type shared: bool
+	:param index: if > 0 append to the end of name. used to make multiple
+				  instances of the same prefs file. If zero it will not
+				  append anything for backwards compatibility. Defaults to 0
+	:type index: int
+	:rtype: :class:`Preference`
+
+	"""
+	key = str(name).replace(' ', '-').lower()
+	if index > 0:
+		key = '%s%s' % (key, index)
+	pref = Prefs()
+	# pref.setShared(shared)
+	# pref.setCoreName(coreName)
+	# look for a default preference file
+	filename = os.path.join(pref.path(coreName, shared), '%s.pref' % key)
+	pref.load(filename)
+	success = False
+	# if (os.path.exists(filename)):
+	# 	success = pref.load(filename)
+	# if not success:
+	# 	# create default information
+	# 	root = pref.addNode('preferences')
+	# 	root.setAttribute('name', name)
+	# 	root.setAttribute('version', 1.0)
+	# 	root.setAttribute('ui', '')
+	# pref.setName(key)
+	# _cache[key] = pref
+	return pref
+	# return _cache[key]
+
+
 
 class Prefs(object):
 
-	def __init__(self, path="", data={}):
-		self.path = path
+	def __init__(self, filepath="", data={}):
 		self._data = data
-		if self.path:
-			try:
-				with open(self.path, 'r') as fp:
-					self._data = json.load(fp)
-			except ValueError as e:
-				# This may be an xml
-				self._data = self.initializeXML()
+		if filepath:
+			self.load(filepath)
 		self.ignore = self.__dict__.keys()
 		self.ignore.append('ignore')
 		self.setAttrs()
@@ -25,7 +67,7 @@ class Prefs(object):
 	def initializeXML(self):
 		# Use the old preference object ot pull the xml data
 		oldprefs = bp.Preference()
-		oldprefs.load(self.path)
+		oldprefs.load(self.filepath)
 		data = self.extractXMLData(oldprefs)
 		return data
 
@@ -46,8 +88,12 @@ class Prefs(object):
 		for key, value in root.attributeDict().iteritems():
 			# Attempt to store the evaluation of the xml string
 			try:
-				data[key] = eval(value)
-			except NameError as e:
+				# value types, JSON doesn't directly work with this
+				if type(eval(value)) == type:
+					data[key] = value
+				else:
+					data[key] = eval(value)
+			except (NameError, SyntaxError) as e:
 				data[key] = value
 		# Recursively traverse through children
 		if element.children():
@@ -62,6 +108,9 @@ class Prefs(object):
 			indent=4,
 			separators=(',', ': ')
 		)
+
+	def __getitem__(self, key):
+		return self._data[key]
 
 	def __setitem__(self, key, value):
 		self._data[key] = value
@@ -84,9 +133,23 @@ class Prefs(object):
 				break
 		return depth
 
+	def path(self, coreName='', shared=False):
+		""" return the path to the application's prefrences folder """
+		# use the core
+		if (not coreName and blurdev.core):
+			coreName = blurdev.core.objectName()
+		path = ''
+		if shared:
+			path = osystem.expandvars(os.environ['BDEV_PATH_PREFS_SHARED']) % {'username': getpass.getuser()}
+		# if not shared or the path does not exist use the non shared path. This is for user accounts who do not
+		# get network shared preference locations.
+		if not path or not os.path.exists(path):
+			path = osystem.expandvars(os.environ['BDEV_PATH_PREFS'])
+		return os.path.join(path, 'app_%s' % coreName)
+
 	def save(self, path=""):
 		if not path:
-			path = self.path
+			path = self.filepath
 		with open(path, 'w') as fh:
 			json.dump(
 				self.data,
@@ -95,5 +158,16 @@ class Prefs(object):
 				separators=(',', ': ')
 			)
 
-	def load(self):
-		pass
+	def load(self, filepath):
+		self.filepath = filepath
+ 		if self.filepath:
+			try:
+				with open(self.filepath, 'r') as fp:
+					self._data = json.load(fp)
+			except (IOError, ValueError) as e:
+				# Handle if the file is not there
+				if type(e) == IOError:
+					self._data = {}
+					return
+				# This may be an xml
+				self._data = self.initializeXML()
